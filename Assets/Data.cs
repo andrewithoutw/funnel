67c94745a64e3357ede8fe92d693fd21109be1f6
andredavidm <59956316+andredavidm@users.noreply.github.com>
2024-03-25 16:17:09 +0000

    data manager and data

---
+++ Assets/Data.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+
+    public List<Vector3> vertices;
+    public List<int> indices;
+    public List<Vector2> uvs;
+}
